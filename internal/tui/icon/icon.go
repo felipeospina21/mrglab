0b4cf02c7eb2e68571a30dbcd2230768a8ebfc9f
felipe ospina <felipe.ospina.-nd@disney.com>
2024-09-23 17:04:38 -0500

    add icon package

---
+++ internal/tui/icon/icon.go
@@ -0,0 +1,27 @@
+package icon
+
+// Icons from https://www.nerdfonts.com/cheat-sheet
+// nf-oct family
+
+const (
+	Dash           = " "
+	Check          = " "
+	Cross          = " "
+	Clock          = " "
+	Rebase         = " "
+	Edit           = " "
+	Person         = " "
+	Discussion     = " "
+	Gear           = " "
+	Empty          = ""
+	CircleCheck    = " "
+	CircleCross    = " "
+	CirclePause    = " "
+	CircleDash     = " "
+	CircleDot      = " "
+	CirclePlay     = " "
+	CircleSkip     = " "
+	CircleQuestion = " "
+	Circle         = " "
+	Gitlab         = " "
+)
