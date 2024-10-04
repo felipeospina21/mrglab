e53495d9a532336d812d63f4400385fa3c9ee4c9
felipe <felipeospina21@hotmail.com>
2024-10-03 19:02:22 -0500

    move main styles to style pckg

--- internal/tui/app/view.go
+++ internal/tui/app/view.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/lipgloss"
+	"github.com/felipeospina21/mrglab/internal/tui/components/details"
 	"github.com/felipeospina21/mrglab/internal/tui/components/projects"
 	"github.com/felipeospina21/mrglab/internal/tui/components/table"
+	"github.com/felipeospina21/mrglab/internal/tui/style"
 	"github.com/felipeospina21/mrglab/internal/tui/task"
 )
 
 func (m Model) View() string {
 	left := projects.DocStyle.Render(m.Projects.List.View())
+	render := style.MainFrameStyle.Render
+
 	if m.ctx.TaskStatus != task.TaskFinished {
 		m.MergeRequests.Table.W, m.MergeRequests.Table.H = m.getEmptyTableSize()
 		body := lipgloss.JoinHorizontal(0, left, table.DocStyle.Render(m.MergeRequests.Table.View()))
 		sl := m.Statusline.View()
-		return MainFrameStyle.Render(lipgloss.JoinVertical(0, body, sl))
+		return render(lipgloss.JoinVertical(0, body, sl))
 
 	}
 
@@ -26,15 +30,14 @@ func (m Model) View() string {
 	if m.ctx.IsLeftPanelOpen {
 		main := lipgloss.JoinHorizontal(0, left, body)
 		sl := m.Statusline.View()
-		return MainFrameStyle.Render(lipgloss.JoinVertical(0, main, sl))
+		return render(lipgloss.JoinVertical(0, main, sl))
 
 	}
 
-	h := m.ctx.Window.Height - lipgloss.Height(body) - MainFrameStyle.GetVerticalFrameSize()
+	h := m.ctx.Window.Height - lipgloss.Height(body) - style.MainFrameStyle.GetVerticalFrameSize()
 	sl := lipgloss.PlaceVertical(h, lipgloss.Bottom, m.Statusline.View())
 	if m.ctx.IsRightPanelOpen {
-		render := lipgloss.NewStyle().MarginTop(1).Border(lipgloss.NormalBorder()).Render
-		right := render(
+		right := details.PanelStyle.Render(
 			fmt.Sprintf(
 				"%s\n%s\n%s",
 				m.Details.HeaderView("Header"),
@@ -43,8 +46,8 @@ func (m Model) View() string {
 			),
 		)
 		main := lipgloss.JoinHorizontal(0, body, right)
-		return MainFrameStyle.Render(lipgloss.JoinVertical(0, main, sl))
+		return render(lipgloss.JoinVertical(0, main, sl))
 	}
 
-	return MainFrameStyle.Render(lipgloss.JoinVertical(0, body, sl))
+	return render(lipgloss.JoinVertical(0, body, sl))
 }
