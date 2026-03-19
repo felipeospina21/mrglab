965f3de9e5ed9b6959f9984a0025e543eff57f74
felipe ospina <felipe.ospina.-nd@disney.com>
2026-03-19 16:52:06 -0500

    update modal styling to have an overlay

--- internal/tui/app/view.go
+++ internal/tui/app/view.go
@@ -13,55 +13,57 @@ import (
 func (m Model) View() string {
 	left := projects.DocStyle.Render(m.Projects.List.View())
 	render := style.MainFrameStyle.Render
+	screen := m.renderNormalScreen(left, render)
+
+	if m.isModalOpen {
+		m.setHelpKeys(modal.Keybinds)
+		return m.Modal.View(screen)
+	}
+
+	return screen
+}
+
+func (m Model) renderNormalScreen(left string, render func(...string) string) string {
 	isInitialScreen := m.taskStatus == taskIdle
 	isFetching := m.taskStatus == taskStarted
 
-	switch {
-	case isInitialScreen:
+	if isInitialScreen {
 		if m.taskErr != nil {
 			body := lipgloss.JoinHorizontal(0, left, m.taskErr.Error())
 			sl := m.Statusline.View()
 			return render(lipgloss.JoinVertical(0, body, sl))
 		}
-
 		m.MergeRequests.Table.W = m.layout.MainPanel.Width - table.DocStyle.GetHorizontalFrameSize() - tableBorderX
 		m.MergeRequests.Table.H = m.layout.MainPanel.Height - tableBorderY
 		tbl := table.DocStyle.Render(m.MergeRequests.Table.View())
 		body := lipgloss.JoinHorizontal(0, left, tbl)
 		sl := m.Statusline.View()
 		return render(lipgloss.JoinVertical(0, body, sl))
-
-	case m.isModalOpen:
-		m.setHelpKeys(modal.Keybinds)
-		body := m.Modal.View()
-		sl := m.Statusline.View()
-		return render(lipgloss.JoinVertical(0, body, sl))
-
-	default:
-		body, sl := m.getMainPanelComponents()
-
-		if m.isLeftOpen {
-			if isFetching {
-				textStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(style.Violet[300])).Render
-				body = fmt.Sprintf("\n %s%s%s\n\n",
-					m.Spinner.View(),
-					" ",
-					textStyle("Fetching..."),
-				)
-			}
-			main := lipgloss.JoinHorizontal(0, left, body)
-			sl := m.Statusline.View()
-			return render(lipgloss.JoinVertical(0, main, sl))
-		}
-
-		if m.isRightOpen {
-			right := m.Details.View()
-			main := lipgloss.JoinHorizontal(0, body, right)
-			return render(lipgloss.JoinVertical(0, main, sl))
-		}
-
-		return render(lipgloss.JoinVertical(0, body, sl))
 	}
+
+	body, sl := m.getMainPanelComponents()
+
+	if m.isLeftOpen {
+		if isFetching {
+			textStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(style.Violet[300])).Render
+			body = fmt.Sprintf("\n %s%s%s\n\n",
+				m.Spinner.View(),
+				" ",
+				textStyle("Fetching..."),
+			)
+		}
+		main := lipgloss.JoinHorizontal(0, left, body)
+		sl := m.Statusline.View()
+		return render(lipgloss.JoinVertical(0, main, sl))
+	}
+
+	if m.isRightOpen {
+		right := m.Details.View()
+		main := lipgloss.JoinHorizontal(0, body, right)
+		return render(lipgloss.JoinVertical(0, main, sl))
+	}
+
+	return render(lipgloss.JoinVertical(0, body, sl))
 }
 
 func (m Model) getMainPanelComponents() (string, string) {
